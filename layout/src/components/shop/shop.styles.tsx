47ca68538f443db23d45ab21fefc58072108be29
Mauricio Alejandro Iniesta Zarza <mauricio.iniesta@elektra.com.mx>
2022-10-03 19:24:58 -0500

    Primer Avance

---
+++ layout/src/components/shop/shop.styles.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+`
+
+export const TituloVerde = styled.span`
+    width: 513px;
+    height: 54px;
+    left: 383px;
+    top: 88px;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 36px;
+    line-height: 54px;
+    text-align: center;
+    color: #01AD35;
+
+    .negrita{
+        font-weight: bold;
+    }
+`
+
+export const TituloNegro = styled.span`
+    width: 513px;
+    height: 54px;
+    left: 383px;
+    top: 88px;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 36px;
+    line-height: 54px;
+    text-align: center;
+    color: #1C2833;
+`
+
+export const Descripcion = styled.span`
+    
+`
